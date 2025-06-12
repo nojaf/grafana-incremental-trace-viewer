b65e3765d6c5174b40cf332927d889b52f422e6c
nojaf <florian.verdonck@outlook.com>
2025-06-12 17:06:40 +0200

    Add frontend code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,23 +2,20 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AppRootProps } from '@grafana/data';
 import { ROUTES } from '../../constants';
-const PageOne = React.lazy(() => import('../../pages/PageOne'));
-const PageTwo = React.lazy(() => import('../../pages/PageTwo'));
-const PageThree = React.lazy(() => import('../../pages/PageThree'));
-const PageFour = React.lazy(() => import('../../pages/PageFour'));
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const TraceOverview = React.lazy(() => import('../../pages/TraceOverview'));
+const TraceDetail = React.lazy(() => import('../../pages/TraceDetail'));
+const queryClient = new QueryClient();
 
 function App(props: AppRootProps) {
   return (
-    <Routes>
-      <Route path={ROUTES.Two} element={<PageTwo />} />
-      <Route path={`${ROUTES.Three}/:id?`} element={<PageThree />} />
-
-      {/* Full-width page (this page will have no side navigation) */}
-      <Route path={ROUTES.Four} element={<PageFour />} />
-
-      {/* Default page */}
-      <Route path="*" element={<PageOne />} />
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path={`${ROUTES.TraceDetails}/:traceId/:spanId`} element={<TraceDetail />} />
+        {/* Default page */}
+        <Route path="*" element={<TraceOverview />} />
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/pages/TraceOverview.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { css } from '@emotion/css';
+import { GrafanaTheme2 } from '@grafana/data';
+import { useStyles2 } from '@grafana/ui';
+import { prefixRoute } from '../utils/utils.routing';
+import { ROUTES } from '../constants';
+import { testIds } from '../components/testIds';
+import { lastValueFrom } from 'rxjs';
+import { PluginPage, getBackendSrv } from '@grafana/runtime';
+import { Link } from 'react-router-dom';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import plugin from '../plugin.json';
+
+type simpleTrace = {
+  traceId: string;
+  spanId: string;
+  timestamp: string;
+  name: string;
+};
+
+type rootTracesResponse = {
+  traces: simpleTrace[];
+};
+
+function TraceOverview() {
+  const _s = useStyles2(getStyles);
+  const result = useSuspenseQuery<simpleTrace[]>({
+    queryKey: ['traces'],
+    queryFn: () =>
+      new Promise(async (resolve, _) => {
+        const response = await getBackendSrv().fetch<rootTracesResponse>({
+          url: `/api/plugins/${plugin.id}/resources/traces`,
+        });
+        const value = await lastValueFrom(response);
+        console.log(value.data);
+        resolve(value.data.traces);
+      }),
+  });
+
+  return (
+    <PluginPage>
+      <div data-testid={testIds.pageOne.container}>
+        This is the trace overview page. We would need to add filters here ourselves.
+        <ul>
+          {result.data.map((r) => {
+            return (
+              <Link key={r.spanId} to={prefixRoute(`${ROUTES.TraceDetails}/${r.traceId}/${r.spanId}`)}>
+                <li>
+                  {r.name} ({r.spanId})
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+    </PluginPage>
+  );
+}
+
+export default TraceOverview;
+
+const getStyles = (theme: GrafanaTheme2) => ({
+  marginTop: css`
+    margin-top: ${theme.spacing(2)};
+  `,
+});
