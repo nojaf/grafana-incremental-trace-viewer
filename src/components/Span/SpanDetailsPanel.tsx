05bb31a1f9c44a3d2bf99b46acb85a572ef885c3
nojaf <florian.verdonck@outlook.com>
2025-07-15 13:44:24 +0200

    Chunk tag selection

--- src/components/Span/SpanDetailsPanel.tsx
+++ src/components/Span/SpanDetailsPanel.tsx
@@ -4,71 +4,57 @@ import clsx from 'clsx';
 import type { SpanInfo } from '../TraceDetail';
 import { mkUnixEpochFromNanoSeconds, formatUnixNanoToDateTime, formatDuration } from 'utils/utils.timeline';
 import { useQuery } from '@tanstack/react-query';
-import { searchTags, search } from 'utils/utils.api';
+import { searchTags, search, KeyValue, AnyValue } from 'utils/utils.api';
 
-const flattenObject = (obj: any, prefix = ''): Record<string, any> => {
-  const flattened: Record<string, any> = {};
-
-  // Handle array of key-value pairs with typed values
-  if (Array.isArray(obj)) {
-    obj.forEach((item: any) => {
-      if (item && typeof item === 'object' && 'key' in item && 'value' in item) {
-        const key = item.key;
-        const value = item.value;
-
-        // Extract the actual value based on the type
-        let actualValue: any = null;
-        if (typeof value === 'object' && value !== null) {
-          // Check for type-specific value fields
-          if ('intValue' in value) {
-            actualValue = parseInt(value.intValue, 10);
-          } else if ('stringValue' in value) {
-            actualValue = value.stringValue;
-          } else if ('boolValue' in value) {
-            actualValue = value.boolValue;
-          } else if ('doubleValue' in value) {
-            actualValue = parseFloat(value.doubleValue);
-          } else if ('bytesValue' in value) {
-            actualValue = value.bytesValue;
-          } else {
-            // Fallback to JSON string if unknown type
-            actualValue = JSON.stringify(value);
-          }
-        } else {
-          actualValue = value;
-        }
-
-        const newKey = prefix ? `${prefix}.${key}` : key;
-        flattened[newKey] = actualValue;
-      }
-    });
-  } else if (typeof obj === 'object' && obj !== null) {
-    // Handle regular nested objects
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        const newKey = prefix ? `${prefix}.${key}` : key;
-
-        if (Array.isArray(obj[key])) {
-          // Handle arrays with bracket notation
-          obj[key].forEach((item: any, index: number) => {
-            const arrayKey = prefix ? `${prefix}[${index}]` : `[${index}]`;
-            if (typeof item === 'object' && item !== null) {
-              Object.assign(flattened, flattenObject(item, arrayKey));
-            } else {
-              flattened[arrayKey] = item;
-            }
-          });
-        } else if (typeof obj[key] === 'object' && obj[key] !== null) {
-          Object.assign(flattened, flattenObject(obj[key], newKey));
-        } else {
-          flattened[newKey] = obj[key];
-        }
-      }
+async function getTagAttributes(
+  datasourceUid: string,
+  start: number,
+  end: number,
+  traceId: string,
+  spanId: string,
+  tags: string[]
+) {
+  // There could potentially be a lot of tags, so we need to split them into groups to avoid the query being too long.
+  const groups: string[][] = [];
+  let currentGroup: string[] = [];
+  let currentLength = 0;
+  let maxLength = 1000;
+  for (const tag of tags) {
+    if (currentLength + tag.length < maxLength) {
+      currentGroup.push(tag);
+      currentLength += tag.length;
+    } else {
+      groups.push(currentGroup);
+      currentGroup = [tag];
+      currentLength = tag.length;
     }
   }
 
-  return flattened;
-};
+  if (currentGroup.length > 0) {
+    groups.push(currentGroup);
+  }
+
+  const promises = groups.map(async (group) => {
+    const q = `{ trace:id = "${traceId}" && span:id = "${spanId}" } | select (${group
+      .map((t) => `span.${t}`)
+      .join(', ')})`;
+    const data = await search(datasourceUid, q, start, end, 1);
+    return data.traces?.[0].spanSets?.[0].spans?.[0].attributes || [];
+  });
+
+  const results: KeyValue[][] = await Promise.all(promises);
+  const combined: Record<string, AnyValue> = {};
+  for (const result of results) {
+    for (const keyValue of result) {
+      if (keyValue.key) {
+        combined[keyValue.key] = keyValue.value !== undefined ? keyValue.value : { stringValue: '???' };
+      }
+    }
+  }
+  console.log('combined', combined);
+
+  return combined;
+}
 
 export const SpanDetailPanel = ({
   span,
@@ -86,49 +72,41 @@ export const SpanDetailPanel = ({
       const start = mkUnixEpochFromNanoSeconds(span.startTimeUnixNano);
       const end = mkUnixEpochFromNanoSeconds(span.endTimeUnixNano);
       const tags = await searchTags(datasourceUid, qTags, start, end);
+      // TODO: spit this into multiple requests as it can be too big
       console.log('TAGS', tags);
-      const q = `{ trace:id = "${span.traceId}" && span:id = "${span.spanId}" } | select (${tags
-        .map((t) => `span.${t}`)
-        .join(', ')})`;
-      const data = await search(datasourceUid, q, start, end, 1);
-      if (data?.traces?.[0]?.spanSets?.[0]?.spans?.[0]?.attributes) {
-        return data.traces[0].spanSets[0].spans[0].attributes;
-      }
-
-      return {};
+      return getTagAttributes(datasourceUid, start, end, span.traceId, span.spanId, tags);
     },
   });
 
-  const formatValue = (value: any, typeOverride?: string) => {
-    const type = typeOverride || typeof value;
-    switch (type) {
-      case 'string':
-        return <span className="px-2 py-2 text-cyan-400">&quot;{value}&quot;</span>;
-      case 'number':
-        return <span className="px-2 py-2 text-blue-500">{value}</span>;
-      case 'boolean':
-        return <span className="px-2 py-2 text-green-600">{value.toString()}</span>;
-      case 'object':
-        return <span className="px-2 py-2 text-gray-200 italic">{JSON.stringify(value)}</span>;
-      default:
-        return <span className="px-2 py-2 text-gray-200 italic">{JSON.stringify(value)}</span>;
+  const formatValue = (value: AnyValue) => {
+    if (value.stringValue !== undefined) {
+      return <span className="px-2 py-2 text-cyan-400">&quot;{value.stringValue}&quot;</span>;
+    } else if (value.boolValue !== undefined) {
+      return <span className="px-2 py-2 text-blue-500">{value.boolValue ? 'true' : 'false'}</span>;
+    } else if (value.intValue !== undefined) {
+      return <span className="px-2 py-2 text-green-600">{value.intValue}</span>;
+    } else if (value.doubleValue !== undefined) {
+      return <span className="px-2 py-2 text-green-600">{value.doubleValue}</span>;
+    } else if (value.bytesValue !== undefined) {
+      return <span className="px-2 py-2 text-gray-200 italic">{JSON.stringify(value)}</span>;
     }
+    return <span className="px-2 py-2 text-gray-200 italic">{JSON.stringify(value)}</span>;
   };
 
-  const basicSpanData = [
-    { key: 'Name', value: span.name },
-    { key: 'ID', value: span.spanId },
-    { key: 'Trace ID', value: span.traceId },
-    { key: 'Start Time', value: formatUnixNanoToDateTime(span.startTimeUnixNano) },
-    { key: 'End Time', value: formatUnixNanoToDateTime(span.endTimeUnixNano) },
-    { key: 'Duration', value: formatDuration(span.endTimeUnixNano - span.startTimeUnixNano), type: 'number' },
+  const basicSpanData: KeyValue[] = [
+    { key: 'Name', value: { stringValue: span.name } },
+    { key: 'ID', value: { stringValue: span.spanId } },
+    { key: 'Trace ID', value: { stringValue: span.traceId } },
+    { key: 'Start Time', value: { stringValue: formatUnixNanoToDateTime(span.startTimeUnixNano) } },
+    { key: 'End Time', value: { stringValue: formatUnixNanoToDateTime(span.endTimeUnixNano) } },
+    { key: 'Duration', value: { stringValue: formatDuration(span.endTimeUnixNano - span.startTimeUnixNano) } },
   ];
 
   const rowClassName = (index: number) => {
     return clsx('leading-7', index % 2 === 0 ? 'bg-gray-800' : 'bg-gray-700');
   };
 
-  const flattenedAttributes = result.data ? flattenObject(result.data) : {};
+  const flattenedAttributes = result.isSuccess ? result.data : {};
 
   return (
     <div className="z-10">
@@ -141,7 +119,7 @@ export const SpanDetailPanel = ({
                   <span className="px-2 py-2">{item.key}</span>{' '}
                   {/* TODO: padding & margins are overriden to 0 by the global CSS and it is not possible to set it on the td tag */}
                 </td>
-                <td>{formatValue(item.value, item.type)}</td>
+                <td>{item.value && formatValue(item.value)}</td>
               </tr>
             ))}
           </tbody>
